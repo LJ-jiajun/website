4cd9eeceb472bde72d738b99609315524fc16a2a
LJ-jiajun <lingjunhj@163.com>
2018-09-30 17:49:05 +0800

    update

--- newhyMall/js/index.js
+++ newhyMall/js/index.js
@@ -1,5 +1,21 @@
+
+document.onload = function(){
+	loginInfo();
+//	console.log();
+}
+
+var Storage = window.localStorage;
+
 $(".pro .pt-nav li").click(function(){
 	if((this).className==""){
 		$(this).addClass("current").siblings().removeClass("current");
 	}
 });
+
+loginInfo();
+function loginInfo(){
+	var loginUser = Storage.getItem("loginUser");
+	loginUser = (new Function('','return '+loginUser))();
+	console.log(loginUser);
+}
+
